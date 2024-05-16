e58ff5c272af590c2b2f9df6d51b53f723271128
Skander Gasmi <Skandergasmi7@gmail.com>
2024-05-16 20:02:41 +0100

    interacting with the gemini pro model using text prompt

--- gemini-start.js
+++ gemini-start.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv"
-import { GoogleGenerativeAI } from "@google/generative-ai"
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-dotenv.config()
+dotenv.config();
 
 
-const genAI = new GoogleGenerativeAI(process.env.API_KEY)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
